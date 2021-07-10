41bcedf9e65c14b3e55580a61e40ed110660b701
Shone Sonawane <sdshone@gmail.com>
2021-07-10 15:23:19 +0530

    checkout page added

--- shop/urls.py
+++ shop/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:id>/', views.detail, name='detail')
+    path('<int:id>/', views.detail, name='detail'),
+    path('checkout/', views.checkout, name='checkout')
 ] #+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- shop/views.py
+++ shop/views.py
@@ -12,7 +12,7 @@ def index(request):
 		product_objects = product_objects.filter(title__icontains=item_name)
 
 	#pagination
-	paginator = Paginator(product_objects,2)
+	paginator = Paginator(product_objects,4)
 	page = request.GET.get('page')
 	product_objects = paginator.get_page(page)
 
@@ -21,3 +21,7 @@ def index(request):
 def detail(request, id):
 	product_object = Product.objects.get(id=id)
 	return render(request, 'shop/detail.html', {'product_object': product_object})
+
+
+def checkout(request):
+	return render(request, 'shop/checkout.html')
